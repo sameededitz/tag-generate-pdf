8de10d33447dfa79aead807b8855e8db817b2428
Sameed <sameedhassan22@gmail.com>
2025-08-03 16:14:17 +0500

    Remove unused 'view' route from expiry tag generator

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,3 @@ Route::middleware(['guest'])
     ->group(function () {
         Route::get('/generate', ExpiryTagGenerator::class)->name('expiry-tag-generator');
     });
-
-Route::get('/view', function () {
-    return view('exports.pdf', [
-        'title' => 'Sample Title',
-        'address' => '123 Sample Address',
-        'mfg_date' => now()->toDateString(),
-        'exp_date' => now()->addDays(30)->toDateString(),
-        'quantity' => 10,
-    ]);
-})->name('view.expiry-tag-generator');
